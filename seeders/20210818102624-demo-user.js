c3cf2d7b437fd5a8b8075f89681d16bb4d3c2219
Andrews <andrews.mensah@amalitech.org>
2021-08-18 10:31:11 +0000

    seeders error rectified

--- seeders/20210818102624-demo-user.js
+++ seeders/20210818102624-demo-user.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('admin', [{
+    return queryInterface.bulkInsert('Users', [{
       email: 'p.q.andy.ashong@gmail.com',
       password: 'e1687253f2e388edcc8e40d6ef1a68ffe644b8a7451eac113d93dbfa492a9d17',
       createdAt: new Date(),
@@ -11,6 +11,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('admin', null, {});
+    return queryInterface.bulkDelete('Users', null, {});
   }
 };
